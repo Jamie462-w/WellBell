a876baf0fe0bb8a8cd9ecd00e4342df1ef9bd998
Jamie462-w <jamie.justin.m@gmail.com>
2024-03-21 14:29:26 +0100

    Initial Commit

---
+++ wellbell/wellbellApp.swift
@@ -0,0 +1,17 @@
+//
+//  wellbellApp.swift
+//  wellbell
+//
+//  Created by Jamie Maier on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct wellbellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
